a83be4a691565c6f5f6dc407257830c372fdd8f5
wesllyakatsuka <wesllyakatsuka@gmail.com>
2018-11-10 04:12:35 -0200

    Utilizando Express ao invés de http

--- 4.node/index.js
+++ 4.node/index.js
@@ -1,36 +1,32 @@
 
 
-const http = require('http');
+const express = require ('express');
 
-let server = http.createServer((req, res)=>{
+let app = express();
 
-console.log('URL: ' , req.url);
-console.log('METHOD: ', req.method);
+app.get('/', (req, res)=>{
 
-switch (req.url){
-	case '/':
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
 	res.end('<h1>Olá</h1>');
-	break;
-
-	case '/users':
-	res.statusCode = 200;
-	res.setHeader('Contetnt-Type', 'application/json');
-	res.end(JSON.stringify({
-		users:[{
-			name: 'Weslly',
-			email: 'wesllyakasuka@hotmail.com',
-			id:7
-		}]
-	}));
-}
- 
-res.end('ok');
 
 });
 
-server.listen(3000, '127.0.0.1', ()=>{
+app.get('/users', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Contetnt-Type', 'application/json');
+	res.json({
+		users: [{
+			name: 'Weslly',
+			email: 'wesllyakasuka@hotmail.com',
+			id: 1
+		}]
+	});
+
+});
+
+
+app.listen(3000, '127.0.0.1', ()=>{
 
 console.log('servidor rodando!');
 
