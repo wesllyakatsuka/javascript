db0f49285aa746331975d7c3ba01b09e84d6dec2
wesllyakatsuka <wesllyakatsuka@gmail.com>
2018-11-10 15:56:36 -0200

    Carregando rotas com o consign

--- 4.node/index.js
+++ 4.node/index.js
@@ -1,17 +1,9 @@
-
-
 const express = require ('express');
-let routesIndex = require('./routes/index');
-let routesUsers = require('./routes/users');
+const consign = require ('consign');
 
 let app = express();
 
-app.use(routesIndex);
-app.use('/users',routesUsers);
-
-
-
-
+consign().include('routes').into(app);
 
 app.listen(3000, '127.0.0.1', ()=>{
 
--- 4.node/routes/users.js
+++ 4.node/routes/users.js
@@ -1,27 +1,26 @@
+module.exports = (app) => {
 
-let express = require('express')
-let routes = express.Router();
-
-routes.get('/', (req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Contetnt-Type', 'application/json');
-	res.json({
-		users: [{
-			name: 'Weslly',
-			email: 'wesllyakasuka@hotmail.com',
-			id: 13
-		}]
+	app.get('/users', (req, res) => {
+		res.statusCode = 200;
+		res.setHeader('Contetnt-Type', 'application/json');
+		res.json({
+			users: [{
+				name: 'Weslly',
+				email: 'wesllyakasuka@hotmail.com',
+				id: 7
+			}]
+		});
+	
 	});
-
-});
-
-routes.get('/admin', (req, res) => {
-    res.statusCode = 200;
-	res.setHeader('Contetnt-Type', 'application/json');
-	res.json({
-		users: []
+	
+	app.get('/users/admin', (req, res) => {
+		res.statusCode = 200;
+		res.setHeader('Contetnt-Type', 'application/json');
+		res.json({
+			users: []
+		});
+	
 	});
+	
+};
 
-})
-
-module.exports = routes;
