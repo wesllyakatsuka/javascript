bf46c0935b9bbe1a839b539d2ef402c38c8556c6
wesllyakatsuka <wesllyakatsuka@gmail.com>
2018-11-13 17:22:25 -0200

    Persistência de dados com o NeDB

--- 4.node/routes/users.js
+++ 4.node/routes/users.js
@@ -1,3 +1,10 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+	filename: 'users.db',
+	autoload: true
+})
+
+
 module.exports = (app) => {
 
 	app.get('/users', (req, res) => {
@@ -15,7 +22,18 @@ module.exports = (app) => {
 	
 	app.post('/users', (req, res) => {
 
-		res.json(req.body);
+		db.insert(req.body,(err, user) => {
+
+			if(err){
+				console.log('error: ${err}');
+				res.status(400).json({
+					error: err
+				});
+				
+			} else {
+				res.status(200).json(user);
+			}
+		});
 		
 		
 	
