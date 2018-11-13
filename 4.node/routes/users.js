615f23afce0514877e60db8772f998a2bf0dbe12
wesllyakatsuka <wesllyakatsuka@gmail.com>
2018-11-13 20:01:23 -0200

    Obtendo dados de um usuário com findOne

--- 4.node/routes/users.js
+++ 4.node/routes/users.js
@@ -35,13 +35,27 @@ module.exports = (app) => {
 				app.utils.error.send(err, req, res);
 
 			} else {
-				res.status(200).json(user);
+				res.status(200).json(user); 
 			}
 		});
-		
-		
-	
+
 	});
+		
+let routeId = app.route('/users/:id');
+ 
+routeId.get((req, res) => {
+
+	db.findOne({_id:req.params.id}).exec((err, user)=>{
+
+		if(err){
+			app.utils.error.send(err, req, res);
+
+		} else {
+			res.status(200).json(user); 
+		}
+	});
+
+
+});
 	
 };
-
