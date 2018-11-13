5ecf73096a3743979b20e3033a31950c5bac8b7d
wesllyakatsuka <wesllyakatsuka@gmail.com>
2018-11-13 19:39:10 -0200

    Refatorando e otimizando o código

--- 4.node/routes/users.js
+++ 4.node/routes/users.js
@@ -7,15 +7,13 @@ let db = new NeDB({
 
 module.exports = (app) => {
 
-	app.get('/users', (req, res) => {
+	let route = app.route('/users');
+
+	route.get((req, res) => {
 
 		db.find({}).sort({nome: 1}).exec((err, users)=>{
 			if(err){
-				console.log('error: ${err}');
-				res.status(400).json({
-					error:err
-				});
-
+				app.utils.error.send(err, req, res);
 			}else{
 				res.statusCode = 200;
 				res.setHeader('Contetnt-Type', 'application/json');
@@ -29,16 +27,13 @@ module.exports = (app) => {
 
 	});
 	
-	app.post('/users', (req, res) => {
+	route.post((req, res) => {
 
 		db.insert(req.body,(err, user) => {
 
 			if(err){
-				console.log('error: ${err}');
-				res.status(400).json({
-					error: err
-				});
-				
+				app.utils.error.send(err, req, res);
+
 			} else {
 				res.status(200).json(user);
 			}
