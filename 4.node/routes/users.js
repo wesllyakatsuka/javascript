e225b08e8791f473b8789b22c7dd55f208c48e97
wesllyakatsuka <wesllyakatsuka@gmail.com>
2018-11-13 17:44:11 -0200

    Listando usuários do banco NeDB

--- 4.node/routes/users.js
+++ 4.node/routes/users.js
@@ -2,22 +2,31 @@ let NeDB = require('nedb');
 let db = new NeDB({
 	filename: 'users.db',
 	autoload: true
-})
+});
 
 
 module.exports = (app) => {
 
 	app.get('/users', (req, res) => {
-		res.statusCode = 200;
-		res.setHeader('Contetnt-Type', 'application/json');
-		res.json({
-			users: [{
-				name: 'Weslly',
-				email: 'wesllyakasuka@hotmail.com',
-				id: 7
-			}]
+
+		db.find({}).sort({nome: 1}).exec((err, users)=>{
+			if(err){
+				console.log('error: ${err}');
+				res.status(400).json({
+					error:err
+				});
+
+			}else{
+				res.statusCode = 200;
+				res.setHeader('Contetnt-Type', 'application/json');
+				res.json({
+					
+					users: users
+				});
+			}
+
 		});
-	
+
 	});
 	
 	app.post('/users', (req, res) => {
